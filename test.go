06d88ebaacc7de2089c2b7681fa1b9a987611b70
Joseph Liao <jliao@godaddy.com>
2024-05-16 14:24:36 -0400

    second commit

--- test.go
+++ test.go
@@ -1,39 +1,57 @@
 package main
 
 import (
+    "bufio"
     "fmt"
+    "log"
     "os"
-	"log"
-	"bufio"
-	"unicode"
+    "unicode"
 )
 
 func main() {
-	file, err := os.Open("a.txt")
-	if err != nil {
+    file, err := os.Open("a.txt")
+    if err != nil {
         log.Fatalf("failed to open file: %s", err)
     }
     defer file.Close()
+
     fmt.Println("Hello, World!")
 
-	scanner := bufio.NewScanner(file)
-	ans := 0
+    scanner := bufio.NewScanner(file)
+    ans := 0
 
-	for scanner.Scan() {
-		line := scanner.Text()
-		fmt.Println(line)
+    for scanner.Scan() {
+        line := scanner.Text()
+        fmt.Println(line)
 
-		sum := 0
-		for _, char := range line {
-			if unicode.IsDigit(char) {
-				sum = sum * 10 + int(char - '0')
-			}
-		}
-		fmt.Println(sum)
-		ans += sum
+        var first, second int
+        foundFirst, foundSecond := false, false
 
-	}
+        for _, char := range line {
+            if unicode.IsDigit(char) {
+                first = int(char - '0')
+                foundFirst = true
+                break
+            }
+        }
 
-	fmt.Println(ans)
+        for i := len(line) - 1; i >= 0; i-- {
+            char := rune(line[i])
+            if unicode.IsDigit(char) {
+                second = int(char - '0')
+                foundSecond = true
+                break
+            }
+        }
 
+        if foundFirst && foundSecond {
+            ret := first*10 + second
+            fmt.Printf("First digit: %d, Second digit: %d, Result: %d\n", first, second, ret)
+            ans += ret
+        } else {
+            fmt.Println("Could not find two digits in the line.")
+        }
+    }
+
+    fmt.Println("Final answer:", ans)
 }
